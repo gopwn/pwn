813671e12bf732a8f602bdb9af7d80bb2a738257
UlisseMini <ulisse.mini@gmail.com>
2019-01-05 22:55:25 -0500

    added misc file with ToBytes function

---
+++ misc.go
@@ -0,0 +1,22 @@
+// misc.go contains random useful functions
+
+package pwn
+
+// ToByte takes type interface{} and converts it to []byte, if it can't convert
+// to []byte it will panic
+func ToByte(t interface{}) (output []byte) {
+	switch x := t.(type) {
+	case string:
+		output = []byte(x)
+	case []byte:
+		output = x
+	case byte:
+		output = append(data, x)
+	case rune:
+		output = []byte(string(x))
+	default:
+		panic("failed to convert t to type []byte")
+	}
+
+	return output
+}

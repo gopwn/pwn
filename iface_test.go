3b7c3739da14f2ebb0a7157697a032114266807a
UlisseMini <ulisse.mini@gmail.com>
2019-01-06 23:16:54 -0500

    made tests for iface.go better

--- iface_test.go
+++ iface_test.go
@@ -1,29 +1,43 @@
 package pwn
 
-import "testing"
-import "fmt"
+import (
+	"reflect"
+	"testing"
+)
+
+// TODO: implement mock of []net.Interface for testing
+/*
+	interfaces := []net.Interface{
+		{
+			Index: 1,
+			MTU: 313,
+			Name: "test iface",
+			HardwareAddr: net.HardwareAddr,
+			Flags: net.Flags,
+		}
+	}
+	ifaces, err := getInterfaceAddrs(interfaces)
+	if err != nil {
+		t.Fatal(err)
+	}
+*/
 
 func TestGetAllIfaceAddrs(t *testing.T) {
-	// TODO: better tests, currently only tests the function not crash / panicing
-	// Not much we can 'test' though since it doesnt take input except
-	// GetIFaceByName and GetIFaceByIndex
-	GetAllIfaceAddrs()
-
-	// Few strings for testing this function
-	testcasesName_str := [4]string{"lo", "eth0", "wlan0", "fake"}
-	
-	for i := 0; i < len(testcasesName_str); i++ {
-		fmt.Printf("\nTestcase: %d | GetIFaceByName(\"%s\") | \n", i, testcasesName_str[i])
-		fmt.Println( GetIFaceByName(testcasesName_str[i]) )
+	// currently only test that the function can be called
+	ifaces, err := GetInterfaceAddrs()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(ifaces) < 1 {
+		t.Skip("len(ifaces) < 1")
 	}
 
-
-	// Few ints for testing this function
-	testcasesIndex_int := [8]int{0, 1, 2, 3, 4, 5, 6, 7}
-
-	for i := 0; i < len(testcasesIndex_int); i++ {
-		fmt.Printf("\nTestcase: %d | GetIFaceByIndex(%d) | \n", i, testcasesIndex_int[i])
-		fmt.Println( GetIFaceByIndex(testcasesIndex_int[i]) )
+	iface, err := GetIFaceByName(ifaces[0].Name)
+	if err != nil {
+		t.Fatal(err)
 	}
 
+	if !reflect.DeepEqual(iface, ifaces[0]) {
+		t.Fatal("iface != faces[0]")
+	}
 }

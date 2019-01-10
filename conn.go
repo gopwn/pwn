396e3f0527d1b013f7ebe3a92c97b391fc9d2621
UlisseMini <ulisse.mini@gmail.com>
2019-01-10 10:51:32 -0500

    struct embedding is kool

--- conn.go
+++ conn.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"net"
 	"sync"
-	"time"
 )
 
 // ErrMaxLen indecates that the max length was reached for ReadTill
@@ -13,85 +12,18 @@ var ErrMaxLen = errors.New("max length reached")
 // Conn is a generic stream-oriented network connection.
 //
 // Multiple goroutines may invoke methods on a Conn simultaneously.
-type Conn interface {
-	// Read reads data from the connection.
-	// Read can be made to time out and return an Error with Timeout() == true
-	// after a fixed time limit; see SetDeadline and SetReadDeadline.
-	Read(b []byte) (n int, err error)
-
-	// Write writes data to the connection.
-	// Write can be made to time out and return an Error with Timeout() == true
-	// after a fixed time limit; see SetDeadline and SetWriteDeadline.
-	Write(b []byte) (n int, err error)
-
-	// Close closes the connection.
-	// Any blocked Read or Write operations will be unblocked and return errors.
-	Close() error
-
-	// LocalAddr returns the local network address.
-	LocalAddr() net.Addr
-
-	// RemoteAddr returns the remote network address.
-	RemoteAddr() net.Addr
-
-	// SetDeadline sets the read and write deadlines associated
-	// with the connection. It is equivalent to calling both
-	// SetReadDeadline and SetWriteDeadline.
-	//
-	// A deadline is an absolute time after which I/O operations
-	// fail with a timeout (see type Error) instead of
-	// blocking. The deadline applies to all future and pending
-	// I/O, not just the immediately following call to Read or
-	// Write. After a deadline has been exceeded, the connection
-	// can be refreshed by setting a deadline in the future.
-	//
-	// An idle timeout can be implemented by repeatedly extending
-	// the deadline after successful Read or Write calls.
-	//
-	// A zero value for t means I/O operations will not time out.
-	SetDeadline(t time.Time) error
-
-	// SetReadDeadline sets the deadline for future Read calls
-	// and any currently-blocked Read call.
-	// A zero value for t means Read will not time out.
-	SetReadDeadline(t time.Time) error
-
-	// SetWriteDeadline sets the deadline for future Write calls
-	// and any currently-blocked Write call.
-	// Even if write times out, it may return n > 0, indicating that
-	// some of the data was successfully written.
-	// A zero value for t means Write will not time out.
-	SetWriteDeadline(t time.Time) error
-
-	// ReadLine reads until '\n' and returns bytes read and possible error.
-	ReadLine() ([]byte, error)
-
-	// ReadTill reads till delim and returns bytes read and possible error.
-	ReadTill(delim byte) ([]byte, error)
-
-	// MaxLen sets the maximum length for ReadTill and ReadLine.
-	MaxLen(length int)
-
-	// TLDR just supply a string or bytes
-	// WriteLine writes a line to the connection.
-	// t can be anything convertable to []byte (see ToBytes function)
-	// ToBytes will panic if it fails to convert to bytes
-	WriteLine(t interface{}) error
-}
-
-// conn is the underlying struct returned by pwn.Dial etc
-type conn struct {
-	// c is the underlying connection
-	c net.Conn
-
-	// mu is used for protecting struct variables from concurrent reads / writes
-	mu sync.Mutex
+type Conn struct {
+	// the embeedded net.Conn
+	net.Conn
 
 	// the max length for ReadLine and ReadTill.
 	maxLen int
+
+	// mu is used for protecting struct variables from concurrent reads / writes
+	mu sync.Mutex
 }
 
-func (c *conn) MaxLen(length int) {
+func (c *Conn) MaxLen(length int) {
 	// prevent panics
 	if c == nil {
 		return
@@ -103,70 +35,19 @@ func (c *conn) MaxLen(length int) {
 }
 
 // ReadLine reads until '\n' and returns bytes read and possible error.
-func (c *conn) ReadLine() ([]byte, error) {
+func (c *Conn) ReadLine() ([]byte, error) {
 	return ReadTill(c, c.maxLen, '\n')
 }
 
 // ReadTill reads till 'delim' and returns bytes read and possible error.
-func (c *conn) ReadTill(delim byte) ([]byte, error) {
+func (c *Conn) ReadTill(delim byte) ([]byte, error) {
 	return ReadTill(c, c.maxLen, delim)
 }
 
 // TLDR just supply a string or []byte
-// WriteLine writes a line to the connection.
+// WriteLine writes a line to the Connection.
 // t can be anything convertable to []byte (see ToBytes function)
 // ToBytes will panic if it fails to convert to bytes
-func (c *conn) WriteLine(t interface{}) error {
+func (c *Conn) WriteLine(t interface{}) error {
 	return WriteLine(c, t)
 }
-
-// Below are the methods for the net.Conn interface.
-
-// Read reads data from the connection.
-// Read can be made to time out and return an Error with Timeout() == true
-// after a fixed time limit; see SetDeadline and SetReadDeadline.
-func (c *conn) Read(buf []byte) (int, error) { return c.c.Read(buf) }
-
-// Write writes data to the connection.
-// Write can be made to time out and return an Error with Timeout() == true
-// after a fixed time limit; see SetDeadline and SetWriteDeadline.
-func (c *conn) Write(data []byte) (int, error) { return c.c.Write(data) }
-
-// Close closes the connection.
-// Any blocked Read or Write operations will be unblocked and return errors.
-func (c *conn) Close() error { return c.c.Close() }
-
-// LocalAddr returns the local network address.
-func (c *conn) LocalAddr() net.Addr { return c.c.LocalAddr() }
-
-// RemoteAddr returns the remote network address.
-func (c *conn) RemoteAddr() net.Addr { return c.c.RemoteAddr() }
-
-// SetDeadline sets the read and write deadlines associated
-// with the connection. It is equivalent to calling both
-// SetReadDeadline and SetWriteDeadline.
-//
-// A deadline is an absolute time after which I/O operations
-// fail with a timeout (see type Error) instead of
-// blocking. The deadline applies to all future and pending
-// I/O, not just the immediately following call to Read or
-// Write. After a deadline has been exceeded, the connection
-// can be refreshed by setting a deadline in the future.
-//
-// An idle timeout can be implemented by repeatedly extending
-// the deadline after successful Read or Write calls.
-//
-// A zero value for t means I/O operations will not time out.
-func (c *conn) SetDeadline(t time.Time) error { return c.c.SetDeadline(t) }
-
-// SetReadDeadline sets the deadline for future Read calls
-// and any currently-blocked Read call.
-// A zero value for t means Read will not time out.
-func (c *conn) SetReadDeadline(t time.Time) error { return c.c.SetReadDeadline(t) }
-
-// SetWriteDeadline sets the deadline for future Write calls
-// and any currently-blocked Write call.
-// Even if write times out, it may return n > 0, indicating that
-// some of the data was successfully written.
-// A zero value for t means Write will not time out.
-func (c *conn) SetWriteDeadline(t time.Time) error { return c.c.SetWriteDeadline(t) }
--- net.go
+++ net.go
@@ -2,21 +2,78 @@ package pwn
 
 import (
 	"net"
+	"sync"
 )
 
-// Dial creates a new network connection using net.Dial
+// A Listener is a generic network listener for stream-oriented protocols.
+// Multiple goroutines may invoke methods on a Listener simultaneously.
+type Listener interface {
+	// Accept waits for and returns the next Connection to the listener.
+	Accept() (Conn, error)
+
+	// Close closes the listener.
+	// Any blocked Accept operations will be unblocked and return errors.
+	Close() error
+
+	// Addr returns the listener's network address.
+	Addr() net.Addr
+}
+
+type listener struct {
+	l net.Listener
+}
+
+func (l listener) Addr() net.Addr {
+	return l.l.Addr()
+}
+
+func (l listener) Close() error {
+	return l.l.Close()
+}
+
+func (l listener) Accept() (Conn, error) {
+	rawConn, err := l.l.Accept()
+	if err != nil {
+		return Conn{}, err
+	}
+
+	return Conn{
+		rawConn,
+		// the default line length to be used with Conn.ReadLine
+		MaxLenDefault,
+
+		sync.Mutex{},
+	}, nil
+}
+
+// Dial creates a new network Connection using net.Dial
 // then creates a pwn.Conn using it and returns it
 // MaxLineLength is by default set to 256, you can change it in the returned
 // Conn using Conn.MaxLen(i int).
 func Dial(network, addr string) (Conn, error) {
 	rawConn, err := net.Dial(network, addr)
 	if err != nil {
-		return &conn{}, err
+		return Conn{}, err
 	}
 
-	return &conn{
-		c: rawConn,
-		// the default line length to be used with conn.ReadLine
-		maxLen: MaxLenDefault,
+	return Conn{
+		rawConn,
+		// the default line length to be used with Conn.ReadLine
+		MaxLenDefault,
+
+		sync.Mutex{},
+	}, nil
+}
+
+// Listen creates a net.Listener that will accept Connections
+// and wrap them in a pwn.Conn
+func Listen(network, addr string) (Listener, error) {
+	rawListener, err := net.Listen(network, addr)
+	if err != nil {
+		return nil, err
+	}
+
+	return listener{
+		l: rawListener,
 	}, nil
 }

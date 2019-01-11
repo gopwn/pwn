c1f1c8221efd611dd1574c6cbfb79b316d1eedb4
UlisseMini <ulisse.mini@gmail.com>
2019-01-11 11:39:38 -0500

    [Move misc.go into pwn.go]

--- pwn.go
+++ pwn.go
@@ -1,2 +1,21 @@
 // Package pwn implements tools for capture the flag compititions.
 package pwn
+
+// Bytes takes type interface{} and converts it to []byte, if it can't convert
+// to []byte it will panic
+func Bytes(t interface{}) (output []byte) {
+	switch x := t.(type) {
+	case string:
+		output = []byte(x)
+	case []byte:
+		output = x
+	case byte:
+		output = append(output, x)
+	case rune:
+		output = []byte(string(x))
+	default:
+		panic("failed to convert t to type []byte")
+	}
+
+	return output
+}

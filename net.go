608743c4b1af61f11be30fd58fa972b45d651c53
UlisseMini <ulisse.mini@gmail.com>
2019-01-11 11:38:49 -0500

    moved conn.go into net.go

--- net.go
+++ net.go
@@ -46,8 +46,51 @@ func (l listener) Accept() (Conn, error) {
 	}, nil
 }
 
+// Conn is a generic stream-oriented network connection.
+//
+// Multiple goroutines may invoke methods on a Conn simultaneously.
+type Conn struct {
+	// the embeedded net.Conn
+	net.Conn
+
+	// the max length for ReadLine and ReadTill.
+	maxLen int
+
+	// mu is used for protecting struct variables from concurrent reads / writes
+	mu sync.Mutex
+}
+
+// MaxLen changes the max length for ReadLine and ReadTill in the conn
+func (c *Conn) MaxLen(length int) {
+	// prevent panics
+	if c == nil {
+		return
+	}
+
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.maxLen = length
+}
+
+// ReadLine reads until '\n' and returns bytes read and possible error.
+func (c *Conn) ReadLine() ([]byte, error) {
+	return ReadTill(c, c.maxLen, '\n')
+}
+
+// ReadTill reads till 'delim' and returns bytes read and possible error.
+func (c *Conn) ReadTill(delim byte) ([]byte, error) {
+	return ReadTill(c, c.maxLen, delim)
+}
+
+// WriteLine writes a line to the Connection.
+// t can be anything convertable to []byte (see ToBytes function)
+// ToBytes will panic if it fails to convert to bytes
+func (c *Conn) WriteLine(t interface{}) error {
+	return WriteLine(c, t)
+}
+
 // Dial creates a new network Connection using net.Dial
-// then creates a pwn.Conn using it and returns it
+// then creates a pwn.Conn wrapping it and returns it
 // MaxLineLength is by default set to 256, you can change it in the returned
 // Conn using Conn.MaxLen(i int).
 func Dial(network, addr string) (Conn, error) {
